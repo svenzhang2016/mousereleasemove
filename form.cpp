ae7d5ec2f43cfaee84a2647bf5f247fbdbfb040a
svenzhang <qqwowo2011@outlook.com>
2022-04-16 15:28:44 +0800

    修改程序名称。

--- form.cpp
+++ form.cpp
@@ -29,14 +29,14 @@ void Form::mousePressEvent(QMouseEvent *event) {
 
 void Form::mouseMoveEvent(QMouseEvent *event) {
   if (m_bDrag) {
-    QPoint distance = event->globalPosition().toPoint() - mouseStartPoint;
+    QPoint distance = event->globalPos() - mouseStartPoint;
     this->move(windowTopLeftPoint + distance);
   }
 }
 
 void Form::mouseReleaseEvent(QMouseEvent *event) {
   m_bDrag = !m_bDrag;
-  mouseStartPoint = event->globalPosition().toPoint();
+  mouseStartPoint = event->globalPos();
   windowTopLeftPoint = this->frameGeometry().topLeft();
 }
 
@@ -44,7 +44,7 @@ bool Form::eventFilter(QObject *watched, QEvent *event) {
   if (event->type() == QEvent::MouseMove) {
     if (watched != nullptr && watched != this && m_bDrag) {
       QMouseEvent *me = dynamic_cast<QMouseEvent *>(event);
-      QPoint distance = me->globalPosition().toPoint() - mouseStartPoint;
+      QPoint distance = me->globalPos() - mouseStartPoint;
       this->move(windowTopLeftPoint + distance);
     }
   }
